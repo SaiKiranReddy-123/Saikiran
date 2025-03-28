94b8e3531e1073e3072b3b9651d68be7b2dcdddc
saikiran <saikiran.p@navayuvabharatinfotech.com>
2025-03-28 17:45:55 +0530

    js_datatypes

---
+++ html/javascrpt/data1_types.js
@@ -0,0 +1,42 @@
+
+// refrence data type
+
+ // array:-
+
+ var apple='apple'
+
+ var banana='banana'
+
+ var cherry = 'cherry'
+
+var A=[1,2,3,apple,banana,cherry,[1,2,3,4],[12.13]]
+
+ console.log(A) 
+
+ console.log(A.length) //8
+
+ var empdata =["sai","deepthi","chandeep"]
+
+ console.log(empdata[0]) // sai
+
+//object:-
+
+nybemp={
+    empname:'sai',
+    empid:9515,
+    phno:9701138513,
+    email:'sai@gmail.com',
+    role:'IT'
+
+}
+
+
+
+
+
+console.log(nybemp)
+
+
+console.log(nybemp.empid)
+
+console.log(nybemp.email)
