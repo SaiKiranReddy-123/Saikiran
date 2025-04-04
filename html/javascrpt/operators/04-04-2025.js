9914e046203abb11c7dce445e70a3addfc6bc9d4
saikiran <saikiran.p@navayuvabharatinfotech.com>
2025-04-04 19:20:36 +0530

    operators

---
+++ html/javascrpt/operators/04-04-2025.js
@@ -0,0 +1,105 @@
+//operators
+
+
+//arthimatic operator
+
+// +
+
+// var a = 10
+
+// var b = 20
+
+// console.log(a+b)   //30
+
+
+// var c = a + b
+
+// console.log(c)
+
+// console.log(c+a)
+
+// var fname='sai'
+
+// var lname='kiran'
+
+// console.log(fname + lname)
+
+
+//   -
+
+// var a = 10
+
+// var b = 20
+
+// console.log(a-b)
+
+// console.log(fname - lname)      //NAN
+
+
+//    *
+
+// var a = 10
+
+// var b = 20
+
+// console.log(10 * 20)
+
+// (/)
+
+// var a = 50
+
+
+// var b = 5
+
+// console.log(a/b)
+
+//  (%)
+
+// var a = 50
+
+
+// var b = 5
+
+// console.log(a%b)
+
+// var a = 50
+
+// //  ++-  , ++-
+// var b = 5
+
+// console.log(++a + b++)
+
+
+// console.log(a)
+
+
+// console.log(b)
+
+// console.log(a++)
+
+
+// //   --a ,  b--
+
+// var a = 10
+
+// var b = 20 
+
+// console.log(a-- + ++a)
+
+// console.log(a-- + --b)
+
+// console.log(a)
+
+
+// assignment operator
+
+
+var a = 10
+
+var b = 20 
+
+b+=a
+
+console.log(b)
+
+
