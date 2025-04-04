e2016c4a57ec4054b24aa5d89cb8c15e0e2c0d58
saikiran <saikiran.p@navayuvabharatinfotech.com>
2025-04-04 14:26:51 +0530

    04-04-2024

---
+++ html/javascrpt/data_types2.js
@@ -0,0 +1,55 @@
+//functions
+//syntax :- function (keyword) function_name(choice) {    }
+
+
+console.log('function')
+
+
+function hello()  {
+
+    var a =10
+    var b = 20
+
+console.log(a)
+
+console.log(b)
+
+
+}
+
+hello()
+
+
+
+//named function
+
+var hello=function hello() {
+
+    console.log('named function')
+
+    var a = 10;
+
+    var b = 10;
+
+    console.log(a)
+
+    console.log(b)
+
+
+
+}
+
+hello()
+
+
+
+var a = function() {
+
+    var a = 15;
+
+    var b = 25;
+
+    console.log(a);
+
+    console.log(b);
+}
