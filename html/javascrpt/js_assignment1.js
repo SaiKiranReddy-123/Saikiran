670057fde501113eeefc569edece7aa74f43a7ef
saikiran <saikiran.p@navayuvabharatinfotech.com>
2025-04-08 10:47:03 +0530

    08-04-25

---
+++ html/javascrpt/js_assignment1.js
@@ -0,0 +1,17 @@
+let average_marks = 85;
+
+if (average_marks >= 90) {
+    console.log('Excellent');
+} 
+else if (average_marks >= 80) {
+    console.log("Good job!");
+} 
+else if (average_marks >= 70) {
+    console.log("Needs improvement.");
+} 
+else if (average_marks >= 60) {
+    console.log("Passing, could do better.");
+} 
+else {
+    console.log("Fail");
+}
---
+++ html/javascrpt/task.js
@@ -0,0 +1,53 @@
+
+//To write a program even or odd
+//-------------------------------
+// Num = 7
+
+
+// if (Num%2==0){
+//     console.log("even number")
+// }
+
+// else{
+//     console.log
+//         ("odd number")
+// }
+
+//write a program to find highest vaule among 4 numbers
+//-------------------------------------------------------
+
+//  var a = 10
+
+//  var b = 20
+
+//  var c = 30
+
+//  var d = 40
+
+//  let highest = a
+
+//  if (b > highest){
+//     highest=b
+//  }
+
+//  if (c > highest){
+//     highest=c
+//  }
+//  if (d > highest){
+//     highest=d
+//  }
+
+//  console.log("the highest number is :" + highest)
+
+
+
+// let num1 = 45;
+// let num2 = 89;
+// let num3 = 23;
+// let num4 = 67;
+
+// let highest = Math.max(num1, num2, num3, num4);
+
+// console.log("The highest number is: " + highest);
+
+
