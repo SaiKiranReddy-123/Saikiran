1db3fe6981ce73cb6db2ecdefad1da9e7cdb9a47
saikiran <saikiran.p@navayuvabharatinfotech.com>
2025-04-09 09:06:40 +0530

    js_task2

---
+++ html/javascrpt/js_assignment2.js
@@ -0,0 +1,21 @@
+
+//
+
+for (i=1;i<=10;i++){
+    console.log(i*7)
+}
+
+
+
+for(i=1;i<=50;i++){
+    if (i%2 !==0){
+        console.log(i)
+    }
+    }
+    
+
+for(i=1;i<=100;i++){
+    console.log(i*5)
+}
+
+
