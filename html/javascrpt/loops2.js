8aaf70a156d1be2ad40825261cbb6f01c809cca3
saikiran <saikiran.p@navayuvabharatinfotech.com>
2025-04-09 18:10:36 +0530

    09-04-25

---
+++ html/javascrpt/loops2.js
@@ -0,0 +1,51 @@
+
+// var person = {
+//     Name:'saikiran',
+//     age:24,
+//     city:'atp'
+// }
+
+// var details=['sai',24,'atp']
+
+
+// console.log(person)
+
+// console.log(details)
+
+// //for in
+
+// for (var x in person){
+//     console.log(x) // x is called a dynamic key
+//     console.log(person[x])
+// }
+
+
+// //for of
+
+// for (var x of details){
+//     console.log(x)
+// }
+
+// for (var x in details){  // --> here we are getting index values.
+//     console.log(x) 
+//     console.log(person[x])
+// }
+
+// for (var x of person){  //  --> here we are getting error like  Type error:"person is not iterable"
+//     console.log(x)         // beacuse symbol iterable not there..
+//     console.log(person[x])  //for of loop performs only on arrays..
+// }
+
+
+// for (i=1; i<=10;i++){
+//     if(i%2 ==0){
+
+//     if(i%4==0){
+
+//         continue    
+      
+//     }
+//     console.log(i);
+//     }
+
+// }
---
+++ html/javascrpt/task3.js
@@ -0,0 +1,90 @@
+// Display Student Data(Using for...in loop)
+//----------------------------------------------
+
+
+// var student={
+
+//     Name:'Mary',
+
+//     Age:25,
+     
+//     class:'fifth',
+
+//     Medium:'english'
+
+// }
+
+// for (var a in student){
+
+//     console.log(student)
+
+// }
+
+
+//Month Display (Using Switch) takes a month number (1-12)?
+//----------------------------------------------------------
+
+
+//     let month=6;
+
+//     switch(month) {
+//         case 1:
+//             monthName = "January";
+//             break;
+//         case 2:
+//             monthName = "February";
+//             break;
+//         case 3:
+//             monthName = "March";
+//             break;
+//         case 4:
+//             monthName = "April";
+//             break;
+//         case 5:
+//             monthName = "May";
+//             break;
+//         case 6:
+//             monthName = "June";
+//             break;
+//         case 7:
+//             monthName = "July";
+//             break;
+//         case 8:
+//             monthName = "August";
+//             break;
+//         case 9:
+//             monthName = "September";
+//             break;
+//         case 10:
+//             monthName = "October";
+//             break;
+//         case 11:
+//             monthName = "November";
+//             break;
+//         case 12:
+//             monthName = "December";
+//             break;
+//         default:
+//             monthName = "Invalid month number";
+//             break;
+//     }
+
+//     console.log(monthName);
+// }
+
+
+// displayMonth(6);  
+
+
+//Create an array that contains the marks of 5 students in an exam Use a for...of loop and calculate the average marks.
+
+// let marks = [95,90,75,80,55];
+
+// let sum = 0;
+
+// for (let x of marks){
+//     sum+=x
+// }
+
+// let avg = sum/marks.length;
+// console.log(avg)
