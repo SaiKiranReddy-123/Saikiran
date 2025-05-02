8a20f7a92e45e88f08640559a963fd397823cc8e
saikiran <saikiran.p@navayuvabharatinfotech.com>
2025-05-02 16:44:32 +0530

    02.05.25

---
+++ java script/asessments/asessments4.js
@@ -0,0 +1,115 @@
+
+// //1
+// function getFirstChar(str){
+//     if(str.length==0){
+//         return 0
+//     }
+//     else{
+//         return str.charAt(0)
+//     }
+
+   
+
+// }
+
+// console.log(getFirstChar(['JAVA SCRIPT']))
+
+
+
+
+//2
+
+
+
+
+// var sum = 0
+
+// function sumArray(arr){
+
+//     for (i of a){
+
+//        sum  = sum+i
+//     }
+// }
+
+// console.log(sumArray([1,2,3]))
+
+
+//  3
+
+function containsValue(arr, value) {
+
+    return arr.includes(value)
+  }
+
+  
+  console.log(containsValue([11,12,13],11))
+
+  //4
+
+
+  function getArrayLength(arr) {
+    let count = 0
+    for (let i in arr) {
+      count++
+    }
+    return count
+  }
+
+  console.log(getArrayLength(['hello',null,10]))
+  
+
+// 6
+
+var num = 10
+
+function isMultipleOfFive(num) {
+  return num % 5 === 0
+}
+
+console.log(isMultipleOfFive(num))
+
+// 4
+
+var arr = [11,12,13,14,15]
+
+function getArrayLength(arr) 
+{
+    return arr.length
+}
+
+
+console.log(getArrayLength(arr))
+
+// 10
+
+// var s = new set ()
+
+// s.sdd(11)
+
+// s.sdd(12)
+
+// s.sdd(13)
+
+// s.sdd(12)
+
+// s.sdd(11)
+
+// console.log(s)
+
+// // 9 
+
+// var arr1 = [1,2,3,4,5]
+
+
+
+
+
+var arr2 = [10,20,30,40,50]
+
+function findMin(arr) {
+
+    console.log(arr2)
+}
+
+console.log(Math.min([arr2]))
